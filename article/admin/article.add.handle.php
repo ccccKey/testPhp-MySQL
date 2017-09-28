8eee867f84c1f1be2c14101042f44ee9df0dc669
ccccKey <670897678@qq.com>
2017-09-28 19:36:47 +0800

    2017-9-28

--- article/admin/article.add.handle.php
+++ article/admin/article.add.handle.php
@@ -1,4 +1,20 @@
 <?php
      require_once('../connect.php');
-     print_r($_POST);
+     
+    if(!(isset($_POST['title']) && (!empty($_POST['title'])))) {
+     	echo "<script>alert('标题不能为空');window.location.href='article.add.php'</script>";
+     } 
+
+     $title = $_POST['title'];
+     $author = $_POST['author'];
+     $description = $_POST['description'];
+     $content = $_POST['content'];
+     $dateline = time();
+
+     $insertSql = "insert into article(title, author, description, content, dateline) values('$title','$author','$description','$content',$dateline)";
+     if(mysqli_query($link,$insertSql)){
+     	echo "<script>alert('插入成功');window.location.href='article.add.php'</script>";
+     }else{
+     	echo "<script>alert('插入失败');window.location.href='article.add.handle.php'</script>";
+     }
 ?>
